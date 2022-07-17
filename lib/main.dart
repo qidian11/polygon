bf3fc802afd0a37f8ccfa006dd137fc5c9cdee70
qidian11 <wangsan1997@gmail.com>
2022-07-18 00:56:17 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+import 'package:polygon/polygon_painter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> _animation;
+  late Tween<double> _tween;
+  late AnimationController _animationController;
+  double sides = 0;
+  double progress = 0.0;
+  double maxSides = 20.0;
+  String sliderText = '0.0';
+  double canvasWidth = 500;
+  bool showDots = false;
+  bool showDiagonal = true;
+  // 是否正在滑动
+  bool isSidesChange = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _tween = Tween(begin: 0.0, end: 1.0);
+    _animationController =
+        AnimationController(duration: const Duration(seconds: 60), vsync: this);
+    _animation = _tween.animate(_animationController)
+      ..addListener(() {
+        setState(() {
+          progress = _animation.value;
+        });
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _animationController.reverse();
+        }
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build');
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Row(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width / 2,
+                color: const Color(0xFFE9EFF2),
+              ),
+              Container(
+                  width: MediaQuery.of(context).size.width / 2,
+                  color: const Color(0xFFD3DBDF))
+            ],
+          ),
+          SingleChildScrollView(
+            child: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 100,
+                    ),
+                    SizedBox(
+                      width: canvasWidth,
+                      height: canvasWidth,
+                      child: CustomPaint(
+                          painter: PolygonPainter(
+                        sides: sides,
+                        radius: canvasWidth / 2,
+                        showDots: showDots,
+                        showDiagonal: showDiagonal,
+                        progress: progress,
+                      )),
+                    ),
+                    const SizedBox(
+                      height: 200,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(left: 24.0, right: 0.0),
+                          child: Text('Show Dots'),
+                        ),
+                        Switch(
+                          value: showDots,
+                          onChanged: (value) {
+                            setState(() {
+                              if (value == true &&
+                                  sides == 20 &&
+                                  !isSidesChange) {
+                                _animationController.repeat(reverse: true);
+                                showDots = value;
+                              } else {
+                                _animationController.stop();
+                                showDots = value;
+                              }
+                            });
+                          },
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(left: 24.0, right: 0.0),
+                          child: Text('Show diagonal'),
+                        ),
+                        Switch(
+                          value: showDiagonal,
+                          onChanged: (value) {
+                            setState(() {
+                              showDiagonal = value;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 500,
+                      height: 20,
+                      child: Slider.adaptive(
+                        value: sides,
+                        max: 20,
+                        mouseCursor: SystemMouseCursors.basic,
+                        activeColor: const Color(0xFF2C343A),
+                        inactiveColor: const Color(0xFF56596B),
+                        onChanged: (_value) {
+                          print("sides:$sides");
+                          print("onChanged : $_value");
+                          updateSlider(_value, "onChanged : $_value",
+                              isChange: true);
+                        },
+                        onChangeStart: (_value) {
+                          print("onChangeStart : $_value");
+                          updateSlider(_value, "onChangeStart : $_value",
+                              isChange: true);
+                        },
+                        onChangeEnd: (_value) {
+                          print("onChangeEnd : $_value");
+                          updateSlider(_value, "onChangeEnd : $_value");
+                        },
+                      ),
+                    )
+                  ]),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void updateSlider(double _value, String text, {bool isChange = false}) {
+    sides = _value;
+    sliderText = text;
+    isSidesChange = isChange;
+    if (isSidesChange) {
+      showDots = false;
+      _animationController.stop();
+    }
+    if (_value == maxSides) {
+      showDots = true;
+      if (_animationController.status != AnimationStatus.forward ||
+          _animationController.status != AnimationStatus.reverse) {
+        _animationController.repeat(reverse: true);
+      }
+    }
+    setState(() {});
+  }
+}
---
+++ lib/polygon_painter.dart
@@ -0,0 +1,238 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class PolygonPainter extends CustomPainter {
+  double radius;
+  double sides;
+  bool showDiagonal;
+  bool showDots;
+  double maxSides = 20.0;
+  double progress;
+  int dotsNum = 10;
+  double dotRadius = 3.0;
+  List<Offset> points = [];
+  PolygonPainter(
+      {this.sides = 0,
+      this.radius = 100,
+      this.progress = 0.0,
+      this.showDiagonal = true,
+      this.showDots = false});
+  @override
+  void paint(Canvas canvas, Size size) {
+    // print('paint');
+    Paint paint = Paint()
+      ..color = const Color(0xFF47484B)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 1
+      ..isAntiAlias = true;
+
+    List<Offset> points = getPolygonPoints(sides);
+    canvas.translate(size.width / 2, size.height / 2);
+    // scale canvas
+    double scale;
+    // 0->8:0.4->0.8 8->12:0.8->0.4 12->20:0.4->1
+    if (sides.ceil() <= 8) {
+      scale = 0.4 + 0.05 * sides;
+    } else if (sides.ceil() <= 12) {
+      scale = 0.8 - 0.1 * (sides - 8);
+    } else {
+      scale = 0.4 + 0.6 / 8 * (sides - 12);
+    }
+    print('scale:$scale,sides:$sides');
+    // 偶变奇，斜率0.1，奇数变偶数，斜率-0.04，初始scale 0.4
+    // double k1 = 0.5;
+    // double k2 = -0.44;
+    // double startScale = 0.4;
+    // if (sides.floor() % 2 == 0) {
+    //   double startY = sides.floor() / 2 * (k1 + k2) + startScale;
+    //   int startX = sides.floor();
+    //   scale = startY + k1 * (sides - startX);
+    // } else {
+    //   double startY = (sides - 1).floor() / 2 * (k1 + k2) + startScale + k1;
+    //   int startX = sides.floor();
+    //   scale = startY + k2 * (sides - startX);
+    // }
+    canvas.scale(scale);
+    paint.color = const Color(0xFFA5CED8);
+    canvas.drawPoints(PointMode.points, points, paint);
+    // draw sides
+    paint.color = const Color(0xFF2C343A);
+    for (int i = 0; i < points.length; i++) {
+      canvas.drawLine(
+          points[i % points.length], points[(i + 1) % points.length], paint);
+    }
+    paint.color = const Color(0xFF47484B);
+    paint.color = const Color(0xFF99ACC2);
+
+    // draw diagonal
+    if (showDiagonal) {
+      for (int i = 0; i < points.length; i++) {
+        for (int j = 0; j < points.length; j++) {
+          if (i < j && i % points.length != (j - 1) % points.length) {
+            canvas.drawLine(
+                points[i % points.length], points[(j) % points.length], paint);
+          }
+        }
+      }
+    }
+
+    // draw dots
+    if (showDots) {
+      PathStorage pathStorage = PathStorage();
+      if (pathStorage.points.isEmpty) {
+        pathStorage.points = points;
+        int maxLineNum = sides * (sides - 1) ~/ 2;
+        pathStorage.pathList = pathStorage.getPathList(maxLineNum, dotsNum);
+      }
+      List<Path> pathList = pathStorage.pathList;
+      for (int i = 0; i < pathList.length; i++) {
+        PathMetric metric = pathList[i].computeMetrics().first;
+        Offset offset =
+            metric.getTangentForOffset(metric.length * progress)!.position;
+        paint.color = Colors.primaries[i];
+        canvas.drawCircle(offset, dotRadius, paint);
+      }
+    }
+  }
+
+  List<Offset> getPolygonPoints1(double sides) {
+    print(sides.ceil());
+    for (int i = 0; i < sides.ceil(); i++) {
+      double x, y;
+      if (sides.ceil() % 2 == 0) {
+        x = radius *
+            sin(lerpDouble(0, (pi / sides), sides - sides.floor())! +
+                i * 2 * pi / sides);
+        y = -radius *
+            cos(lerpDouble(0, (pi / sides), sides - sides.floor())! +
+                i * 2 * pi / sides);
+      } else {
+        x = radius *
+            sin(lerpDouble((pi / sides), 0, sides - sides.floor())! +
+                i * 2 * pi / sides);
+        y = -radius *
+            cos(lerpDouble((pi / sides), 0, sides - sides.floor())! +
+                i * 2 * pi / sides);
+      }
+      points.add(Offset(x, y));
+    }
+    return points;
+  }
+
+  List<Offset> getPolygonPoints(double sides) {
+    for (int i = 0; i < sides.ceil(); i++) {
+      double x, y;
+      if (sides.ceil() % 2 == 0) {
+        if (sides.ceil() == sides) {
+          x = radius * sin((pi / sides) + i * 2 * pi / sides);
+          y = -radius * cos((pi / sides) + i * 2 * pi / sides);
+        } else {
+          x = radius *
+              sin(lerpDouble(0, (pi / sides), sides - sides.floor())! +
+                  i * 2 * pi / sides);
+          y = -radius *
+              cos(lerpDouble(0, (pi / sides), sides - sides.floor())! +
+                  i * 2 * pi / sides);
+        }
+      } else {
+        if (sides.ceil() == sides) {
+          x = radius * sin(i * 2 * pi / sides);
+          y = -radius * cos(i * 2 * pi / sides);
+        } else {
+          if (i == 0) {
+            double startY = -radius * cos(pi / sides);
+            double endY = -radius;
+            x = 0;
+            y = lerpDouble(startY, endY, sides - sides.floor())!;
+          } else {
+            x = radius *
+                sin(lerpDouble((pi / sides), 0, sides - sides.floor())! +
+                    (i - lerpDouble(1, 0, sides - sides.floor())!) *
+                        2 *
+                        pi /
+                        sides);
+            y = -radius *
+                cos(lerpDouble((pi / sides), 0, sides - sides.floor())! +
+                    (i - lerpDouble(1, 0, sides - sides.floor())!) *
+                        2 *
+                        pi /
+                        sides);
+          }
+        }
+      }
+      points.add(Offset(x, y));
+    }
+    return points;
+  }
+
+  List<Offset> getPolygonPoints2(double sides) {
+    print(sides.ceil());
+    for (int i = 0; i < sides.ceil(); i++) {
+      double x, y;
+
+      x = radius * sin(i * 2 * pi / sides);
+      y = -radius * cos(i * 2 * pi / sides);
+
+      points.add(Offset(x, y));
+    }
+    return points;
+  }
+
+  @override
+  bool shouldRepaint(PolygonPainter oldDelegate) {
+    return oldDelegate.sides != sides ||
+        oldDelegate.showDots != showDots ||
+        oldDelegate.showDiagonal != showDiagonal ||
+        oldDelegate.progress != progress;
+  }
+}
+
+class PathStorage {
+  List<Offset> points = [];
+  List<Path> pathList = [];
+  static PathStorage instance = PathStorage._internal();
+  factory PathStorage() {
+    return instance;
+  }
+  PathStorage._internal();
+
+  List<Path> getPathList(int maxLineNum, int dotsNum) {
+    // 单条path中的line数量
+    List<Path> pathList = [];
+    List<Offset> pointList = [];
+    int lineNum;
+
+    print("maxLineNum:$maxLineNum");
+    for (int i = 0; i < dotsNum; i++) {
+      lineNum = 1 + Random().nextInt(maxLineNum + 1);
+      Path path = Path();
+      for (int j = 0; j < lineNum; j++) {
+        Offset point = getRandomPoint();
+        if (j == 0) {
+          path.moveTo(point.dx, point.dy);
+        } else if (j == 1) {
+          while (point == pointList[0]) {
+            point = getRandomPoint();
+          }
+          path.lineTo(point.dx, point.dy);
+        } else {
+          while (point == pointList[j - 2] || point == pointList[j - 1]) {
+            point = getRandomPoint();
+          }
+          path.lineTo(point.dx, point.dy);
+        }
+        pointList.add(point);
+      }
+      pathList.add(path);
+    }
+    return pathList;
+  }
+
+  Offset getRandomPoint() {
+    int index = Random().nextInt(points.length);
+    Offset point = points[index];
+    return point;
+  }
+}
