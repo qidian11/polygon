539f78c3ebb68275d585414af4626935fabe4400
qidian11 <wangsan1997@gmail.com>
2022-07-20 00:32:27 +0800

    line loading add rotation

--- lib/line_loading_painter.dart
+++ lib/line_loading_painter.dart
@@ -6,6 +6,7 @@ class LineLoadingPainter extends CustomPainter {
   double totalLength;
   double progress;
   double maxProgress;
+  double lineSpace;
   double get lineLength => totalLength / lineNum;
   // 起伏宽度是总长度的1/2
   double get waveLength => totalLength / 2;
@@ -15,7 +16,8 @@ class LineLoadingPainter extends CustomPainter {
       {this.totalLength = 500.0,
       this.lineNum = 40,
       this.progress = 0.0,
-      this.maxProgress = 20.0});
+      this.maxProgress = 20.0,
+      this.lineSpace = 4.0});
   @override
   void paint(Canvas canvas, Size size) {
     Paint paint = Paint()
@@ -23,21 +25,29 @@ class LineLoadingPainter extends CustomPainter {
       ..strokeWidth = 4;
     canvas.translate(size.width / 2, size.height / 2);
     Offset p1 = Offset(-totalLength / 2, 0);
-    Offset p2 = Offset(p1.dx + lineLength - 4, 0);
+    Offset p2 = Offset(p1.dx + lineLength - lineSpace, 0);
     double height = 0;
+    // 斜率
+    double gradient = 0;
+    double angle = 0.0;
     double centerX;
     double waveStartX = progress / maxProgress * (waveLength + totalLength) +
         (-totalLength / 2 - waveLength);
     for (int i = 0; i < lineNum; i++) {
       p1 = Offset(-totalLength / 2 + lineLength * i, 0);
-      p2 = Offset(p1.dx + lineLength - 4, 0);
+      p2 = Offset(p1.dx + lineLength - lineSpace, 0);
       centerX = (p1.dx + p1.dx + lineLength) / 2;
-      print(
-          'i:$i,centerX:$centerX,waveStartX:$waveStartX,waveStartX + waveLength:${waveStartX + waveLength}');
+      // print(
+      //     'i:$i,centerX:$centerX,waveStartX:$waveStartX,waveStartX + waveLength:${waveStartX + waveLength}');
       if (waveStartX < centerX && centerX < (waveStartX + waveLength)) {
         height =
             -sin((centerX - waveStartX) * 2 * pi / waveLength) * waveHeight;
-        print('${((centerX - waveStartX) * pi / waveLength) / pi}');
+        gradient = -cos((centerX - waveStartX) * 2 * pi / waveLength) *
+            waveHeight *
+            2 *
+            pi /
+            waveLength;
+        angle = atan(gradient);
         // } else if (waveStartX < p1.dx && p1.dx < (waveStartX + waveLength)) {
         //   height = -sin((p1.dx - waveStartX) * pi / waveLength) * waveLength / 2;
         //   print('${((p1.dx - waveStartX) * pi / waveLength) / pi}');
@@ -45,12 +55,18 @@ class LineLoadingPainter extends CustomPainter {
         //   height = -sin((p2.dx - waveStartX) * pi / waveLength) * waveLength / 2;
         //   print(((p2.dx - waveStartX) * pi / waveLength) / pi);
       } else {
+        gradient = 0;
+        angle = 0;
         height = 0;
       }
       p1 = Offset(p1.dx, height);
       p2 = Offset(p2.dx, height);
-      canvas.drawLine(p1, p2, paint);
 
+      p1 = Offset(centerX - (lineLength / 2) * cos(angle),
+          height - (lineLength / 2) * sin(angle));
+      p2 = Offset(centerX + ((lineLength / 2) - lineSpace) * cos(angle),
+          height + ((lineLength / 2) - lineSpace) * sin(angle));
+      canvas.drawLine(p1, p2, paint);
       // canvas.translate(p2.dx + 4, p2.dy);
       // canvas.rotate((progress / 20.0) * 2 * pi / lineNum);
     }
